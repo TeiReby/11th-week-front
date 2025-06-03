c77fb1f8c361c731b8878589d9b1d6a5b8584459
teireby <dongin1001@snu.ac.kr>
2025-06-03 23:06:05 +0900

    ㅁㄴㅇㄹㄹ

--- src/components/Comment/CommentElement.jsx
+++ src/components/Comment/CommentElement.jsx
@@ -1,10 +1,9 @@
 import { useState, useEffect } from "react";
 import { updateComment, getUser } from "../../apis/api";
-import { getCookie } from "../../utils/cookie";
 
 const CommentElement = (props) => {
-  const { comment, handleCommentDelete, postId } = props;
-  const [content, setContent] = useState(comment.content);
+  const { comment, handleCommentDelete } = props;
+  const [content] = useState(comment.content);
   const [isEdit, setIsEdit] = useState(false);
   const [user, setUser] = useState(null); // state for user
 
